851b48fd5b2f16aeaa14356f7a0f945e6f02e0da
Sargis <sargis.torosyan@esterox.am>
2024-05-19 16:15:56 +0400

    Created create post form

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,14 +5,26 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Form\PostType;
+use App\Entity\Post;
 
 class MainController extends AbstractController
 {
-    #[Route('/main', name: 'app_main')]
+    #[Route('/', name: 'app_main')]
     public function index(): Response
     {
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
+        ]);
+    }
+
+    #[Route('/create-post', name: 'create-post')]
+    public function createPost(): Response
+    {
+        $post = new Post();
+        $form = $this->createForm(PostType::class, $post);
+        return $this->render('main/create-post.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
